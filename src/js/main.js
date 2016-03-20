24d3c9807d2253cffc0c3d793d6904878914092f
Ivan Kuznetsov <themeam7@gmail.com>
2016-03-21 01:59:36 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,29 @@ $('.btn-collapse').on('click', function() {
         .text(isMore ? less : more);
     isMore = !isMore;
 })
+var jelect = $('.js-jelect'),
+    option = $('.js-jelect-option'),
+    current = $('.js-jelect-current'),
+    type;
+
+jelect.on( 'change', function (e) {
+    current[0].className = 'choose-lang-mob__current js-jelect-current choose-lang-mob__current-' + this.value;
+});
+
+$(function(){
+	$(".menu__dropdown > li > a.trigger").on("click",function(e){
+		var current=$(this).next();
+		var grandparent=$(this).parent().parent();
+		if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+			$(this).toggleClass('right-caret left-caret');
+		grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+		grandparent.find(".sub-menu:visible").not(current).hide();
+		current.toggle();
+		e.stopPropagation();
+	});
+	$(".menu__dropdown > li > a:not(.trigger)").on("click",function(){
+		var root=$(this).closest('.dropdown');
+		root.find('.left-caret').toggleClass('right-caret left-caret');
+		root.find('.sub-menu:visible').hide();
+	});
+});
